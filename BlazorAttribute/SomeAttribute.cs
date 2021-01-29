cd0211f07bd29cef3ca66061ee1c9e2fda26b171
Richard Davies <rathga@gmail.com>
2021-01-29 21:47:28 +0000

    Add project files.

---
+++ BlazorAttribute/SomeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BlazorAttribute
+{
+    public class SomeAttribute : Attribute
+    {
+        public SomeAttribute(SomeEnum something)
+        {
+            Something = something;
+        }
+
+        public SomeEnum Something { get; }
+    }
+}
---
+++ BlazorAttribute/SomeClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAttribute
+{
+    [Some(SomeEnum.Something)] // <-- comment this to make blazor WASM debugging work
+    public class SomeClass
+    {
+        
+    }
+}
